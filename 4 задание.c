bb01ecf7dc7956f6348aafe79b415e5e90569346
sux9i <belakinatatana035@gmail.com>
2026-09-14 23:51:06 +0300

    Add files via upload

---
+++ 4 задание.c
@@ -0,0 +1,30 @@
+﻿﻿#include <stdio.h>
+#include <locale.h>
+
+void name()
+{
+    puts("* * * * * * * * * * * * * * * * * * * * * *");
+    puts("*                                         *");
+    puts("* тема: Разработка консольного приложения *");
+    puts("*                                         *");
+    puts("*     Выполнила Белякина Т.Д.             *");
+    puts("*                                         *");
+    puts("* * * * * * * * * * * * * * * * * * * * * *");
+}
+
+void date()
+{
+puts(" _        _   _    _   _ ");
+puts(" _| |_|  | | |_|  | | |_|");
+puts("|_    |  |_|  _|  |_| |_|");
+}
+
+int main()
+{
+    setlocale(LC_CTYPE, "RUS");
+
+    name();
+    date();
+
+    return 0;
+}
